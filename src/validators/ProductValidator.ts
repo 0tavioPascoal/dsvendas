23ef516b7564322fb7ba00603053dcfe73a1d27f
OtavioPascoal <fjoooosa@gmail.com>
2025-01-10 09:28:32 -0300

    "Feat: customization of validation messages"

--- src/validators/ProductValidator.ts
+++ src/validators/ProductValidator.ts
@@ -1,8 +1,10 @@
 import * as yup from 'yup';
 
+const defaultMessage = "Mandatory Field"
+
 export const ProductValidationSchema = yup.object().shape({
-  name: yup.string().required(),
-  description: yup.string().required(),
-  sku: yup.string().required(),
-  price: yup.number().required()
+  name: yup.string().trim().required(defaultMessage),
+  description: yup.string().trim().required(defaultMessage),
+  sku: yup.string().trim().required(defaultMessage),
+  price: yup.number().required(defaultMessage).moreThan(0 , "Value must be greater than 0")
 })
